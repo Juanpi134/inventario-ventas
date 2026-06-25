f16a1a0dfba0ad01d153bf65275cede7d3e8b6a5
Juanpi134 <juanpitocino_15@hotmail.com>
2026-06-25 09:29:53 -0300

    importar el componente navbar al main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Productos from "./Productos";
 import Ventas from "./Ventas";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./componentes/Navbar";
 
 function App(){
   return (
    <BrowserRouter>
-
+  <Navbar />
       <Routes>
         <Route path="/productos" element={<Productos />} />
         <Route path="/ventas" element={<Ventas />} />
