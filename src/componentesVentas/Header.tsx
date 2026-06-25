cec3ca1a94988777c1c482f08359647f06534ab6
Juanpi134 <juanpitocino_15@hotmail.com>
2026-06-25 14:18:30 -0300

    nuevos cambios con los componentes ventas

--- src/componentesVentas/Header.tsx
+++ src/componentesVentas/Header.tsx
@@ -1,10 +1,11 @@
+import  '../estilos/header.css'
 type HeaderProps = {
   titulo: string;
 };
 
 export default function Header({titulo}:HeaderProps){
     return (
-        <div className="header">
+        <div className="ventas-header">
             <h1>{titulo}</h1>
         </div>
     )
